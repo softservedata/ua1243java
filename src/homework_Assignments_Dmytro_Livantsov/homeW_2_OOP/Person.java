c210024451f896ca5ce2d9943259f86cf2f2eac7
Dmitry_Livantsov <dimalivantsov173@gmail.com>
2024-08-07 21:44:19 +0100

    Homework update

--- src/homework_Assignments_Dmytro_Livantsov/homeW_2_OOP/Person.java
+++ src/homework_Assignments_Dmytro_Livantsov/homeW_2_OOP/Person.java
@@ -45,6 +45,33 @@ public class Person {
     }
 
 
+    // Getter and Setter
+    //
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+
     @Override
     public String toString() {
         return "Person{" +
