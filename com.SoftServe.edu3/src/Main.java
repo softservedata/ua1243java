7ecbd1340e742662c349dc072fb57d311e8d22df
KhrystynaTs <64132994+KhrystynaTs@users.noreply.github.com>
2024-08-08 23:50:59 +0300

    Update Main.java
    better version

--- com.SoftServe.edu3/src/Main.java
+++ com.SoftServe.edu3/src/Main.java
@@ -1,16 +1,28 @@
+
+
+package persons;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
-
 public class Main {
+   static public final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Person person3= new Person();
-        Person person1 = new Person("Khrystyna","Martynova",1998);
-        Person person2 = new Person("Oleksandr","Martynov");
+        Scanner scan = new Scanner(System.in);
 
+        Person person1 = new Person();
+        Person.input(person1);
+        Person.output(person1);
 
+        Person person2 = new Person();
+        Person.input(person2);
+        Person.output(person2);
+        Person person3 = new Person();
+        Person.input(person3);
+        Person.output(person3);
 
-
+        }
 
     }
-}
