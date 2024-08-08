3089530032058bc3cf4062dad9286d7c0ac1fc90
Denys Petrovskyi <den_petrovskyi@outlook.com>
2024-08-08 16:24:02 +0300

    Homework and Practice - Lesson2

---
+++ Lesson2_VCS_Gi/src/hw_lesson2.java
@@ -0,0 +1,8 @@
+public class hw_lesson2 {
+    public static void main(String[] args) {
+        System.out.println("Organized my own branch in local and remote repositories to upload tasks.");
+        System.out.println("Created separate folders for practice and homework assignments and upload\n" +
+                "assignments to folders by class number.");
+        System.out.println("Downloaded own completed practice and homework from class 1");
+    }
+}
---
+++ Lesson2_VCS_Gi/src/pr_lesson2.java
@@ -0,0 +1,8 @@
+public class pr_lesson2 {
+    public static void main(String[] args) {
+        System.out.println("Cloned file from the github repo.");
+        System.out.println("Added to file «Zapovit.txt» few lines and commit it to local repository.");
+        System.out.println("Maked branch and checkout to it");
+        System.out.println("Push it to remote repository. Resolve conflict.");
+    }
+}
