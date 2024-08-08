abdb0a20731bc7757d7f5ad97c7c5520a84bda3a
KhrystynaTs <64132994+KhrystynaTs@users.noreply.github.com>
2024-08-08 23:51:34 +0300

    Update Person.java
    better version

--- com.SoftServe.edu3/src/Person.java
+++ com.SoftServe.edu3/src/Person.java
@@ -1,61 +1,91 @@
+package persons;
+
+import static persons.Main.SCANNER;
+
 public class Person {
     private String firstName;
     private String lastName;
     private int birthYear;
 
-    public Person(){
-        firstName = "Roman";
-        lastName = "Svoren";
-        birthYear = getAge(1976);
-        System.out.println(this.output());
+    public Person() {
+        this("N/A","N/A"  );
+}
 
-    }
-
-    public Person(String firstName, String lastName) {
+    public Person(String lastName, String firstName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println(this.output());
-    }
-
-
-
-    public Person(String firstName, String lastName, int birthYear){
-     input(firstName,lastName,birthYear);
-        System.out.println(this.output());
-
 
     }
 
+    static int getAge(int birthYear){
 
-    public int getAge(int birthYear){
-        int age = 2024-birthYear;
-       return age;
-    }
-    public void input(String firstName, String lastName, int birthYear){
-        this.firstName= firstName;
-        this.lastName=lastName;
-        this.birthYear= getAge(birthYear);
-
-    }
-    public  String output(){
-        String information = "First name: "+firstName+". Last name: "+lastName+". The age of person: "+birthYear+"\n";
-   return information;
+        return 2024-birthYear;
     }
 
-    /**
-     *
-     * @param Khrystyna
-     * @param Martynova
-     * @return цей метод зміни імені я не використала в програмі. Бо не знаю джо якого конструктора його приліпити
-     * і за яких умов змінювати ім'я адже дані не вводяться з консолі. Принаймі я так зрозуміла задачу.
-     */
-    public String changeName(String firstName,String lastName){
-        this.firstName=firstName;
-    this.lastName=lastName;
-    return this.output();
+
+    public  static void changeName(String fn, String ln ){
+
+        System.out.println("Should the  name be changed? ");
+        String answer = SCANNER.nextLine();
+        String yes = "Yes";
+        String no = "No";
+        if (answer.equalsIgnoreCase(yes)) {
+            fn = SCANNER.nextLine();
+            //Не знаю як тут далі перезаписати дані
+        }
+        else if (answer.equalsIgnoreCase(no)) {
+            System.out.println("ok,thank you.");
+        }
+        else {
+            System.out.println("Please enter yes or no.");
+        }
+
+    }
+    public static void input (Person person) {
+        System.out.println("Input first name: ");
+        String firstName = SCANNER.nextLine();
+        person.setFirstName(firstName);
+        System.out.println("Input last name: ");
+        String lastName = SCANNER.nextLine();
+        person.setLastName(lastName);
+        System.out.println("Input the birth of year: ");
+        int birthYear = SCANNER.nextInt();
+        SCANNER.nextLine();
+
+        person.setBirthYear(birthYear);
+
+    }
+    static public void output(Person person) {
+        System.out.println("Name: " + person.getFirstName());
+        System.out.println("Last name: " + person.getLastName());
+        System.out.println("Date of birth: " + person.getBirthYear());
+        System.out.println("Age: " + getAge(person.getBirthYear()));
+    }
 
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+
+
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
 }
-    }
-
