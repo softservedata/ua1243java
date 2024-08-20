76bb22dc61187af44708ce334667b889f5bf06c5
Khrystyna <kristina50963@gmail.com>
2024-08-20 10:22:05 +0300

    homework 1

---
+++ Homework1A/src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello and welcome! What is your radius ?");
+        double radius = scan.nextDouble();
+        double area = Math.PI * radius * radius;
+        double length = 2 * Math.PI * radius;
+
+        System.out.println("Area of circle: " + area);
+        System.out.print("Length of circle: " + length);
+        System.out.println("Thank you!");
+
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+    }
+}
