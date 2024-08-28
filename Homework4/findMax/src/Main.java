257668da55e156f9ca517bc877b006bfd0bcae28
oshuty <olya.shutileva@gmail.com>
2024-08-28 17:40:57 +0300

    some comments

--- Homework4/findMax/src/Main.java
+++ Homework4/findMax/src/Main.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*Ініціалізувати maximum і minimum значенням з array[0] не буде працювати коректно,
+ оскільки на початку масив array заповнений нулями,
+ і ти порівнюєш введені значення з нулем, що може призвести до некоректних результатів.*/
 public class Main {
-
 
     public static void main(String[] args) {
 
@@ -12,20 +12,18 @@ public class Main {
         System.out.println("Please, enter the three numbers: ");
 
 
-
         //напишите тут ваш код
         int[] array = new int[3];
         int maximum = array[0];
-        int minimum  = array[0];
+        int minimum = array[0];
 
 
-        for(int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
 
             array[i] = Integer.parseInt(scan.nextLine());
-            if ( array[i] > maximum){
+            if (array[i] > maximum) {
                 maximum = array[i];
-            }
-            else if (array[i] < minimum){
+            } else if (array[i] < minimum) {
                 minimum = array[i];
             }
         }
@@ -33,8 +31,7 @@ public class Main {
         System.out.println("Min value is " + minimum);
 
 
-
-       }
+    }
 
         /*
         if (a > b && b > c) {
@@ -66,4 +63,39 @@ public class Main {
 
 */
 
+}
+//перероблено
+class FindMaxMinValues {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.println("Please, enter three numbers: ");
+        int[] array = new int[3];
+        // Ініціалізація масиву і максимального та мінімального значень першими значеннями, які вводить користувач
+        array[0] = Integer.parseInt(scan.nextLine());
+        int maximum = array[0];
+        int minimum  = array[0];
+        // Введення наступних чисел та оновлення максимальних і мінімальних значень
+        for (int i = 1; i < array.length; i++) { // Починаємо з 1, оскільки 0 вже заповнений
+            array[i] = Integer.parseInt(scan.nextLine());
+            if (array[i] > maximum) {
+                maximum = array[i];
+            }
+            if (array[i] < minimum) {
+                minimum = array[i];
+            }
+        }
+        //як альтернатива
+        // Використання Math.max і Math.min для порівняння значень
+//        for (int i = 1; i < array.length; i++) {
+//            maximum = Math.max(maximum, array[i]);
+//            minimum = Math.min(minimum, array[i]);
+//        }
+
+        System.out.println("Max value is " + maximum);
+        System.out.println("Min value is " + minimum);
+
+        scan.close();
     }
+    //ще можна було додати перевірку чи не є всі три введенні числа однаковими
+}
