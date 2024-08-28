9b0ec93b14aaaf5c85c6c3342f85c63ed41c9b74
oshuty <olya.shutileva@gmail.com>
2024-08-28 14:50:16 +0300

    some comments

--- com.softserve.udu/src/Triangle.java
+++ com.softserve.udu/src/Triangle.java
@@ -1,33 +1,29 @@
 import java.util.Scanner;
 
-
 public class Triangle {
 
-
-    Scanner scan = new Scanner(System.in);
+    static Scanner scan = new Scanner(System.in);//better scan to do as static
     double a = scan.nextInt();
     double b = scan.nextInt();
     double c = scan.nextInt();
-    public void areaOfTriangle(){
-        // Знаходимо площу трикутника за формулою Герона10
-        double halfPerimeter = (a+b+c)/2;
-        double area = Math.sqrt(halfPerimeter*(halfPerimeter-a)*(halfPerimeter-b)*(halfPerimeter-c));
-        String result = String.format("%.2f",area);
-        System.out.println("Area of  a triangle: " +result);
-    }
+
+    public static void main(String[] args) {
 
 
-
-public static void main(String[] args){
-
-    System.out.println("Hello! Please, enter sides of the triangle:");
-
+        System.out.println("Hello! Please, enter sides of the triangle:");
         Triangle obj = new Triangle();
         obj.areaOfTriangle();
+        scan.close();//remember to close resource
+    }
 
-
-
-
-
-}
+    public void areaOfTriangle() {
+        // Знаходимо площу трикутника за формулою Герона10
+        double halfPerimeter = (a + b + c) / 2;
+        double area = Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+        String result = String.format("%.2f", area);
+        System.out.println("Area of  a triangle: " + result);
+    }
+    //Не забувай про форматування коду веред відправкою його на гітхаб
+    // У цій задачі було б добре додати перевірку чи існує трикутник із заданими велечинами сторін, оскільки якщо ввести
+    // наприклад, 3,7,10, то площу поверне просто 0,0
 }
