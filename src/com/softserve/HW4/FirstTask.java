29626c2980544c589d122b6ebc2587878a0de162
Anastasiia <anastasiia.datskooo@gmail.com>
2024-09-17 23:59:18 +0300

    Add 4 homework

--- src/com/softserve/HW4/FirstTask.java
+++ src/com/softserve/HW4/FirstTask.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class FirstTask {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(
