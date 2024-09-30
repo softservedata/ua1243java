b92f7eaa97015cc8e4391581c6086bcbf822d900
Khrystyna <kristina50963@gmail.com>
2024-09-30 12:35:14 +0300

    final project

--- Final2/src/Main.java
+++ Final2/src/Main.java
@@ -10,22 +10,24 @@ public class Main {
     public static final Path FILE_1 = Paths.get("D:\\Java_projects\\file1.txt");
     public static final Path FILE_2 = Paths.get("D:\\Java_projects\\file2.txt");
 
-        public static void main (String[]args) throws IOException {
-            long time = System.currentTimeMillis();
-            findElementsPresentInBothList1();
-            System.out.println(System.currentTimeMillis() - time+" milliseconds");
+    public static void main (String[]args)  {
 
-            long time2 = System.currentTimeMillis();
-            findElementsPresentInBothList2();
-            System.out.println(System.currentTimeMillis() - time2+" milliseconds");
+        long time = System.currentTimeMillis();
+        findElementsPresentInBothList1();
+        System.out.println(System.currentTimeMillis() - time+" milliseconds");
 
-            long time3 = System.currentTimeMillis();
-            findElementsPresentInBothList3();
-            System.out.println(System.currentTimeMillis() - time3+" milliseconds");
-        }
+        long time2 = System.currentTimeMillis();
+        findElementsPresentInBothList2();
+        System.out.println(System.currentTimeMillis() - time2+" milliseconds");
+
+        long time3 = System.currentTimeMillis();
+        findElementsPresentInBothList3();
+        System.out.println(System.currentTimeMillis() - time3+" milliseconds");
+    }
 
 
-    public static void findElementsPresentInBothList1 () throws IOException {
+    public static void findElementsPresentInBothList1 ()  {
+        try{
             List<String> list1 = Files.readAllLines(FILE_1);
             List<String> list2 = Files.readAllLines(FILE_2);
 
@@ -34,30 +36,40 @@ public class Main {
 
             System.out.println("The number of the same line is: "+elementIdenticallyList.size());
             System.out.println(elementIdenticallyList);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-    public static void findElementsPresentInBothList2 () throws IOException {
-        List<String> list1 = Files.readAllLines(FILE_1);
-        List<String> list2 = Files.readAllLines(FILE_2);
-        List<String>  result =  list2.stream()
-                .collect(Collectors.collectingAndThen(Collectors.toSet(),
-                        setView -> setView.isEmpty() ? list1 :
-                                list1.stream().filter(setView::contains)
-                                        .collect(Collectors.toList())));
-        System.out.println("The number of the same line is: "+result.size());
-        System.out.println(result);
     }
-    public static void findElementsPresentInBothList3() throws IOException {
-        List<String> list1 = Files.readAllLines(FILE_1);
-        List<String> list2 = Files.readAllLines(FILE_2);
 
-        Set<String> set2 = new HashSet<>(list2);
-        List<String> result =  list1.stream()
-                .filter(set2::contains)
-                .collect(Collectors.toList());
-        System.out.println("The number of the same line is: "+result.size());
-        System.out.println(result);
+    public static void findElementsPresentInBothList2 ()  {
+        try{
+            List<String> list1 = Files.readAllLines(FILE_1);
+            List<String> list2 = Files.readAllLines(FILE_2);
+            List<String>  result =  list2.stream()
+                    .collect(Collectors.collectingAndThen(Collectors.toSet(),
+                            setView -> setView.isEmpty() ? list1 :
+                                    list1.stream().filter(setView::contains)
+                                            .collect(Collectors.toList())));
+            System.out.println("The number of the same line is: "+result.size());
+            System.out.println(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void findElementsPresentInBothList3()  {
+        try{
+            List<String> list1 = Files.readAllLines(FILE_1);
+            List<String> list2 = Files.readAllLines(FILE_2);
 
+            Set<String> set2 = new HashSet<>(list2);
+            List<String> result =  list1.stream()
+                    .filter(set2::contains)
+                    .collect(Collectors.toList());
+            System.out.println("The number of the same line is: "+result.size());
+            System.out.println(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
